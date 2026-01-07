d0278f448fba037826d4ceef6f23790a2f43a5c5
Eman <emaanshareef77@gmail.com>
2026-01-07 11:08:52 +0500

    remove changes by eman

--- src/data/products.ts
+++ src/data/products.ts
@@ -20,7 +20,7 @@ export interface Product {
 export const products: Product[] = [
   {
     id: "1",
-    name: "Classic Oxford Leather eman",
+    name: "Classic Oxford Leather",
     price: 189,
     originalPrice: 229,
     image: "/highheel.jpg",
